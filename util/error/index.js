3f8fd69b3f06a1081c23f93d16a8cac89ef11501
Shivam Singh <shivamks2712@gmail.com>
2023-01-26 15:57:37 +0530

    Deployed

--- util/error/index.js
+++
@@ -1,9 +0,0 @@
-class ErrorHandler extends Error {
-  constructor(statusCode, message) {
-    super();
-    this.status = statusCode || 500;
-    this.message = message || "Something went wrong. Please try again.";
-  }
-}
-
-module.exports = ErrorHandler;
